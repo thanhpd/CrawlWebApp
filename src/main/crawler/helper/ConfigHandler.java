617f576ea86ab7e8f381035816b587968c410a9f
thanhpd <thanhpd.mac@gmail.com>
2015-12-03 09:39:04 +0700

    XML config file initialization added

--- src/main/crawler/helper/ConfigHandler.java
+++ src/main/crawler/helper/ConfigHandler.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
@@ -13,6 +16,7 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import models.Crawler;
+import models.CrawlerConfig;
 import models.CrawlerSetting;
 import models.Property;
 
@@ -34,6 +38,65 @@ public class ConfigHandler {
 	}
 	
 	private static void createConfigFile() {
+		try {						
+			// Create first crawler configuration
+			CrawlerConfig config1 = new CrawlerConfig();
+			config1.setIncludeBinaryContent(true);
+			config1.setIndividualStorageFolder("crawler1");
+			config1.setMaxPagesToFetch(1000);
+			config1.setMaxDepth(-1);			
+			config1.setConcurrentThread(5);
+			
+			List<String> crawlersDomain1 = new ArrayList<String>();
+			crawlersDomain1.add("https://vi.wikipedia.org/");
+			config1.setCrawlersDomain(crawlersDomain1);
+			
+			List<String> domainSeeders1 = new ArrayList<String>();
+			domainSeeders1.add("https://vi.wikipedia.org/");
+			domainSeeders1.add("https://vi.wikipedia.org/wiki/C%C3%B4ng_ngh%E1%BB%87");
+			config1.setDomainSeeders(domainSeeders1);
+			
+			// Create second crawler configuration
+			CrawlerConfig config2 = new CrawlerConfig();
+			config2.setIncludeBinaryContent(true);
+			config2.setIndividualStorageFolder("crawler1");
+			config2.setMaxPagesToFetch(1000);
+			config2.setMaxDepth(-1);			
+			config2.setConcurrentThread(5);
+			
+			List<String> crawlersDomain2 = new ArrayList<String>();
+			crawlersDomain2.add("http://www.ibm.com/developerworks/vn/");
+			config2.setCrawlersDomain(crawlersDomain2);
+			
+			List<String> domainSeeders2 = new ArrayList<String>();
+			domainSeeders2.add("http://www.ibm.com/developerworks/vn/");			
+			config2.setDomainSeeders(domainSeeders2);	
+			
+			// Create crawler setting and add recent created crawlers' configuration
+			CrawlerSetting setting = new CrawlerSetting();
+			setting.setServerHost("localhost");
+			setting.setCore("new_core");
+			setting.setNumberOfCrawlers(2);
+			
+			List<CrawlerConfig> configs = new ArrayList<CrawlerConfig>();
+			configs.add(config1);
+			configs.add(config2);
+			setting.setConfigs(configs);
+			
+			/* init jaxb marshaler */
+			JAXBContext jaxbContext = JAXBContext.newInstance( CrawlerSetting.class );
+            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+            
+            /* set this flag to true to format the output */
+            jaxbMarshaller.setProperty( Marshaller.JAXB_FORMATTED_OUTPUT, true );
+            
+            /* marshaling of java objects in xml (output to file and standard output) */
+            jaxbMarshaller.marshal( setting, new File( "setting.xml" ) );
+            jaxbMarshaller.marshal( setting, System.out );
+			
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
 //		try {
 //			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 //			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
